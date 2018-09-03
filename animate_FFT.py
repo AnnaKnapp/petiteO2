a3679ef4bd5454f08c16939511601d19c725a96c
unknown <aknapp01@gmail.com>
2018-09-02 20:34:22 -0700

    added fft plot to live animation

--- animate_FFT.py
+++ animate_FFT.py
@@ -13,21 +13,34 @@ import sys
 fileName = sys.argv[1] + '.txt'
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+ax1 = fig.add_subplot(2,1,1)
+ax2 = fig.add_subplot(2,1,2)
+
 
 def animate(i):
     graphdata = open(fileName, 'r').read()
     lines = graphdata.split('\n')
-    xs=[]
-    ys=[]
-    for i in range(1,600):
+    times=[]
+    volts=[]
+    timestep =[]
+        for i in range(1,600):
         line = lines[i*-1]
         if len(line) > 19:
             x,y = line.split(',')
-            xs.append(x)
-            ys.append(y)
+            times.append(x)
+            volts.append(y)
     ax1.clear()
-    ax1.plot(xs,ys)
+    ax1.plot(times,volts)
+
+    for i in range(len(times)-1):
+        timestep.append(times[i+1] - times[i])
+    avgTstep = numpy.average(timestep)
+    print(avgTstep)
+    N = len(volts)
+    voltsFft = numpy.fft.fft(volts)
+    freqs = numpy.linspace(0,1/avgTstep,N)  
+    ax2.clear
+    ax2.plot(freqs, voltsFFT)
 
 ani = animation.FuncAnimation(fig, animate, interval = 1)
 plt.show()
