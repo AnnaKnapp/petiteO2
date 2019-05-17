73dff1b71b1c27697b4fb0731493e9ac238bdf9e
unknown <aknapp01@gmail.com>
2019-05-16 17:48:27 -0700

    made a nernst equation calculator

---
+++ Nernst.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+#define the constants
+kB = np.float64(1.38e-23) #Boltzman Constant
+e0 = np.float64(1.602e-19) #Elementary charge
+
+#define my parameters
+tempK1 = np.float64(873.15)
+tempK2 = np.float64(874.15)
+c1 = np.float64(210000)
+c2 = np.float64(210001)
+c3 = np.float64(210002)
+
+dV1 = (kB*tempK1)/e0 * (np.log(c1/c2))
+dV2 = (kB*tempK2)/e0 * (np.log(c1/c2))
+dV3 = (kB*tempK2)/e0 * (np.log(c1/c3))
+
+print("change in voltage of 1ppm O2 at 600C ")
+print(dV1)
+print("change in voltage of 1ppm O2 at 601C ")
+print(dV2)
+print("change in voltage of 2ppm O2 at 600C ")
+print(dV3)
