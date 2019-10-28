4821ab2d99a70fc21d04506232dbc9fb24990c9c
AnnaKnapp <aknapp01@gmail.com>
2019-10-28 16:01:05 -0700

    working on getting rid of spi errors in continuous conversion mode

--- ads1262/config.py
+++ ads1262/config.py
@@ -4,7 +4,7 @@
 #pinmode is BCM
 STRT = 26 #start pin - output from pi to ADC
 PWDN  = 27 #power down - output from pi to ADC
-DRDY  = 17 #data ready - input from ADC to pi
+DRDY  = 12 #data ready - input from ADC to pi
 
 spiBus = 0 #this selects which pins will be used for the SPI communication. For more information refer to raspberry pi documentation
 clockDiv = 4096 #SPI clock divider - sets speed of spi communication. must be power of 2. Larger vales will mean slower SPI, but extremelylow values may result in errors
--- ads1262/ex_callbacks.py
+++ ads1262/ex_callbacks.py
@@ -1,5 +1,6 @@
 import spidev
 import RPi.GPIO as GPIO
+import pigpio
 from time import sleep
 from time import time
 import sys
@@ -10,12 +11,15 @@ os.nice(-15)
 
 spi = spidev.SpiDev()
 
+pi = pigpio.pi()
+pi.set_mode(adc.DRDY, pigpio.INPUT)
+pi.set_pull_up_down(adc.DRDY, pigpio.PUD_UP)
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(adc.STRT, GPIO.OUT) #start pin at gpio pin 4 - output
-GPIO.setup(adc.DRDY, GPIO.IN, pull_up_down = GPIO.PUD_UP) #DRDY pin
+#GPIO.setup(adc.DRDY, GPIO.IN, pull_up_down = GPIO.PUD_UP) #DRDY pin
 GPIO.setup(adc.PWDN, GPIO.OUT) #PWDN pin
-GPIO.setup(13, GPIO.OUT)
 spi.open(adc.spiBus,0) # (bus, device)??
 spi.mode = 0b01
 spi.max_speed_hz = 1400000000/adc.clockDiv
@@ -32,14 +36,15 @@ adc.read_all_regs()
 
 print("finished")
 
+
+
 GPIO.output(adc.STRT, 1)
 datafile = open(fileName, 'w')
 startime = time()
 errorcount=0
-while 1:
-    incoming = GPIO.wait_for_edge(adc.DRDY, GPIO.FALLING, timeout=100)
+
+def newdata(A,B,C):
     datain = spi.readbytes(6)
-    GPIO.output(13, 1)
     if datain[5] != sum(datain[1:5])+0x9B & 255:
         print("ERR - checksum failed")
     combined_data = datain[1] << 24 | datain[2] << 16 | datain[3] << 8 | datain[4]
@@ -49,7 +54,10 @@ while 1:
     timeSoFar = str(time() - startime)
     stringToWrite = timeSoFar +','+ str(O2_data) + '\n'
     datafile.write(stringToWrite)
-    GPIO.output(13, 0)
-    
+
+while 1:
+    pi.callback(adc.DRDY, pigpio.FALLING_EDGE, takedata)
+
+
 
 
